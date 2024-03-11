e347147aeb5542af08f5483fbe1611ab2c31ada8
Avex Boullevard <159640468+AvexXS@users.noreply.github.com>
2024-03-11 15:07:55 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ echo "⣴⡟⠉⠀⠉⠛⠛⠛⠉⠈⠻⡷"
 
 echo "Setting up Anti Disk Fill...Btw did you know avex is extremely sexy?"
 
-curl -o /var/lib/pterodactyl/antidiskfillscript.sh https://raw.githubusercontent.com/AvexXS/antidiskfill-ptero/main/assets/script.sh
+curl -o /var/lib/pterodactyl/antidiskfillscript.sh https://raw.githubusercontent.com/AvexXS/antidiskfill-ptero/main/script.sh
 sudo chmod -R 777 /var/lib/pterodactyl
 nohup /var/lib/pterodactyl/antidiskfillscript.sh > /dev/null 2>&1 & 
 
